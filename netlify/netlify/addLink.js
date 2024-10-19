d645803a8eda98e28138842be4a74af49cbad5ac
brubot1 <101005033+brubot1@users.noreply.github.com>
2024-10-19 19:51:12 -0300

    Update addLink.js

--- netlify/netlify/addLink.js
+++ netlify/netlify/addLink.js
@@ -1,1 +1,17 @@
+const links = []; // Isso pode ser substituído por uma base de dados
 
+exports.handler = async function (event, context) {
+   if (event.httpMethod === 'POST') {
+      const { link } = JSON.parse(event.body);
+      links.push(link);
+      return {
+         statusCode: 200,
+         body: JSON.stringify({ message: "Link adicionado com sucesso!" }),
+      };
+   }
+
+   return {
+      statusCode: 405,
+      body: JSON.stringify({ message: "Método não permitido" }),
+   };
+};
